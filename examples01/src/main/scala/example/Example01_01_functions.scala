2500362fb023aa64fbfc4c127f6911683d397b38
Sergei Winitzki <winitzki@gmail.com>
2017-10-22 00:12:47 -0700

    shorter code

--- examples01/src/main/scala/example/Example01_01_functions.scala
+++ examples01/src/main/scala/example/Example01_01_functions.scala
@@ -15,8 +15,9 @@ object Example01_01_functions {
     s.toSeq.map(k ⇒ is_even(k)).sum
   }
   def count_even_using_val(s: Set[Int]): Int = {
-    val is_even: Int ⇒ Int = k ⇒ if (k % 2 == 0) 1 else 0
+    val is_even = (k: Int) ⇒ if (k % 2 == 0) 1 else 0
 
+    // Need to convert a `Set[Int]` to a sequence (`Seq[Int]`), or else `map` does not work correctly!
     s.toSeq.map(k ⇒ is_even(k)).sum
   }
 }
