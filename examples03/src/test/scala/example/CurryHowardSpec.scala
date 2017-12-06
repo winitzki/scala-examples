4c29b3ddb816121a2b5d475b8e33c2386a83223e
sergei.winitzki <sergei.winitzki@workday.com>
2017-12-06 01:28:25 -0800

    add tests for the generated functions

--- examples03/src/test/scala/example/CurryHowardSpec.scala
+++ examples03/src/test/scala/example/CurryHowardSpec.scala
@@ -1,6 +1,7 @@
 package example
 
 import example.CHTypes._
+import CurryHoward._
 import org.scalatest.{FlatSpec, Matchers}
 
 class CurryHowardSpec extends FlatSpec with Matchers {
@@ -27,8 +28,6 @@ class CurryHowardSpec extends FlatSpec with Matchers {
   behavior of "syntax of `inhabit`"
 
   it should "compile" in {
-    import CurryHoward._
-
     def f1[X, Y]: X ⇒ Y ⇒ X = inhabit
 
     f1[Int, Boolean] shouldEqual null
@@ -47,75 +46,80 @@ class CurryHowardSpec extends FlatSpec with Matchers {
     LamE(PropE("A", "A"), AppE(PropE("A", "A"), PropE("B", "B"))).propositions shouldEqual Set(PropE("A", "A"), PropE("B", "B"))
   }
 
+  it should "generate correct code for the identity function" in {
+    def f1[X] = ofType[X ⇒ X]
+
+    f1(123) shouldEqual 123
+    f1("abc") shouldEqual "abc"
+    f1(true) shouldEqual true
+  }
+
+  it should "generate correct code for the const function" in {
+    def f2[X, Y] = ofType[X ⇒ Y ⇒ X]
+
+    val cTrue = f2(true)
+    cTrue(123) shouldEqual true
+    cTrue("abc") shouldEqual true
+    cTrue(true) shouldEqual true
+
+    val c3 = f2(3)
+    c3(123) shouldEqual 3
+    c3("abc") shouldEqual 3
+    c3(true) shouldEqual 3
+
+  }
+
   behavior of "type parameter introspection"
 
   it should "get printable representation of enclosing owner's type" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[Int]
 
     result._2 shouldEqual "(<basic>String, <basic>String)"
   }
 
   it should "get printable representation of basic types" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[Int]
 
     result._1 shouldEqual "<basic>Int"
   }
 
   it should "get printable representation of parametric type" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[A]
 
     result._1 shouldEqual "<tparam>A"
   }
 
   it should "get printable representation of function types" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[A ⇒ B]
 
     result._1 shouldEqual "<tparam>A ..=>.. <tparam>B"
   }
 
   it should "get printable representation of fixed types with type constructors" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[Option[Seq[Int]] ⇒ Option[List[Set[A]]] ⇒ B]
 
     result._1 shouldEqual "(1 + <constructor>Seq[Int]) ..=>.. (1 + <constructor>List[Set[A]]) ..=>.. <tparam>B"
   }
 
   it should "get printable representation of fixed types with type constructors with [_]" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[Option[_] ⇒ B]
 
     result._1 shouldEqual "(1 + _) ..=>.. <tparam>B"
   }
 
   it should "get printable representation of Option types" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[Option[A] ⇒ Either[A, B]]
 
     result._1 shouldEqual "(1 + <tparam>A) ..=>.. (<tparam>A + <tparam>B)"
   }
 
   it should "get printable representation of Any, Unit, and Nothing types" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[Any ⇒ Nothing ⇒ Unit]
 
     result._1 shouldEqual "_ ..=>.. 0 ..=>.. 1"
   }
 
   it should "not confuse a type parameter with a type inheriting from Any" in {
-    import CurryHoward._
-
     class Q
 
     def result[A, B, C]: (String, String) = testType[A ⇒ Q]
@@ -124,16 +128,12 @@ class CurryHowardSpec extends FlatSpec with Matchers {
   }
 
   it should "get printable representation of tuple types" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[(Any, Nothing, Unit, A, B, C)]
 
     result._1 shouldEqual "(_, 0, 1, <tparam>A, <tparam>B, <tparam>C)"
   }
 
   it should "get printable representation of tuple of basic types" in {
-    import CurryHoward._
-
     def result[A, B, C]: (String, String) = testType[(Int, String, Boolean, Float, Double, Long, Symbol, Char)]
 
     result._1 shouldEqual "(" + CurryHoward.basicTypes.map("<basic>" + _).mkString(", ") + ")"
