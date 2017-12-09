d614f54a24358cdbbca89e95b699f19a59b8b515
Sergei Winitzki <winitzki@gmail.com>
2017-12-08 22:46:59 -0800

    implement all worked examples

--- examples03/src/test/scala/example/Chapter03_02_examplesSpec.scala
+++ examples03/src/test/scala/example/Chapter03_02_examplesSpec.scala
@@ -1,6 +1,6 @@
 package example
 
-import java.time.LocalDate
+import java.time.{DayOfWeek, LocalDate, LocalDateTime}
 
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -49,7 +49,10 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
     // DayOfWeek.toString is already defined.
     println(s"a is $a, b is $b")
 
-    def isThisSaturday(d: DayOfWeek): Boolean = ???
+    def isThisSaturday(d: DayOfWeek): Boolean = d match {
+      case Saturday => true
+      case _ => false
+    }
 
     isThisSaturday(a) shouldEqual false
     isThisSaturday(b) shouldEqual true
@@ -63,7 +66,7 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
     final case object Wednesday extends DayOfWeek
     final case object Thursday extends DayOfWeek
     final case class Friday(restaurantName: String, amountPaid: Double) extends DayOfWeek
-    final case class Saturday(wakeUp: LocalDate) extends DayOfWeek
+    final case class Saturday(wakeUp: LocalDateTime) extends DayOfWeek
 
     val a: DayOfWeek = Monday
     val b: DayOfWeek = Friday("McDonald’s", 23.49)
@@ -71,7 +74,10 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
     // DayOfWeek.toString is already defined.
     println(s"a is $a, b is $b")
 
-    def howMuchPaid(d: DayOfWeek): Option[Double] = ???
+    def howMuchPaid(d: DayOfWeek): Option[Double] = d match {
+      case Friday(_, amountPaid) ⇒ Some(amountPaid)
+      case _ ⇒ None
+    }
 
     Seq(a, b).map(howMuchPaid) shouldEqual Seq(None, Some(23.49))
   }
@@ -84,8 +90,9 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
 
   final case class TwoRoots(x: Double, y: Double) extends RootsOfQuadratic
 
-  def solve2: ((Double, Double)) ⇒ RootsOfQuadratic = {
-    case (b, c) ⇒ // x^2 + b*x + c == 0
+  val solve2: ((Double, Double)) ⇒ RootsOfQuadratic = {
+    case (b, c) ⇒
+      // x^2 + b*x + c == 0
       val discriminant = b * b - 4.0 * c
       if (discriminant > 0) {
         val x1 = (-b - math.sqrt(discriminant)) / 2
@@ -98,25 +105,28 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
 
   it should "ex03: solve quadratic equation" in {
     Seq(
-      (-2.0, 1.0),
-      (1.0, 1.0),
-      (-1.0, -2.0)
+      (-2.0, 1.0), // x^2 - 2*x + 1 = 0
+      (1.0, 1.0), // x^2 + x + 1 = 0
+      (-1.0, -2.0) // x^2 - x - 2 = 0
     ).map(solve2) shouldEqual Seq(EqualRoots(1.0), NoRealRoots, TwoRoots(-1.0, 2.0))
   }
 
-  def rootAverage(roots: RootsOfQuadratic): Option[Double] = ???
+  def rootAverage(roots: RootsOfQuadratic): Option[Double] = roots match {
+    case NoRealRoots => None
+    case EqualRoots(x) => Some(x)
+    case TwoRoots(x, y) => Some((x + y) / 2)
+  }
 
   it should "ex04: compute rootAverage" in {
-
     rootAverage(TwoRoots(-1.0, 1.0)) shouldEqual Some(0)
     rootAverage(EqualRoots(1.0)) shouldEqual Some(1.0)
     rootAverage(NoRealRoots) shouldEqual None
   }
 
   it should "ex05: solve 100 random equations" in {
-    def getRandom: Double = scala.util.Random.nextDouble() * 2 - 1
+    def random(): Double = scala.util.Random.nextDouble() * 2 - 1
 
-    val coeffs: Seq[(Double, Double)] = Seq.fill(100)((getRandom, getRandom))
+    val coeffs: Seq[(Double, Double)] = Seq.fill(100)((random(), random()))
     val solutions: Seq[RootsOfQuadratic] = coeffs.map(solve2)
 
     val averages: Seq[Option[Double]] = solutions.map(rootAverage)
@@ -125,6 +135,8 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
       case Some(x) ⇒ true
       case None ⇒ false
     }
+    // flatten: Seq[Seq[T]] ⇒ Seq[T]
+    // flatten: Seq[Option[T]] ⇒ Seq[T]
 
     val result1: Seq[Double] = averages.flatten
     val result2: Seq[Double] = averages.collect { case Some(x) ⇒ x }
@@ -144,16 +156,16 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
     def f1[A, B]: ((Option[A], Option[B])) ⇒ Option[(A, B)] = {
       case (maybeA, maybeB) ⇒
         maybeA match {
-          case Some(x) ⇒ maybeB match {
-            case Some(y) ⇒ Some((x, y))
-            case None ⇒ None
+          case Some(a) => maybeB match {
+            case Some(b) => Some((a, b))
+            case None => None
           }
-          case None ⇒ None
+          case None => None
         }
     }
 
     /*
-
+    maybeA : Option[T]
     maybeA match {
       case Some(x) ⇒ Some(f(x))
       case None ⇒ None
@@ -161,13 +173,14 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
 
     is the same as:
 
-    maybeA.map(f)
+    maybeA.map(f) // maybeA map f
 
      */
 
     def f2[A, B]: ((Option[A], Option[B])) ⇒ Option[(A, B)] = {
       case (maybeA, maybeB) ⇒
-        val result: Option[Option[(A, B)]] = maybeA.map(x ⇒ maybeB.map(y ⇒ (x, y)))
+        val result = maybeA.map(x ⇒ maybeB.map(y ⇒ (x, y)))
+        // Option[A] .map ( A => Option[(A, B)] ) : Option[Option[(A, B)]]
         result.flatten
     }
 
@@ -180,9 +193,20 @@ class Chapter03_02_examplesSpec extends FlatSpec with Matchers {
         maybeA.flatMap(x ⇒ maybeB.map(y ⇒ (x, y)))
     }
 
+    def f4[A, B]: ((Option[A], Option[B])) ⇒ Option[(A, B)] = {
+      case (maybeA, maybeB) ⇒
+        val result: Option[Option[(A, B)]] = maybeA.map {a ⇒
+          val c: Option[(A, B)] = maybeB.map(b ⇒ (a, b))
+          c
+        }
+        val r: Option[(A, B)] = result.flatten
+        r
+    }
+
     check(f1)
     check(f2)
     check(f3)
+    check(f4)
   }
 
 }
