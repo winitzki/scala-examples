f6f73b55ff2a0ca082b8828ac9658e62f56e4f23
sergei.winitzki <sergei.winitzki@workday.com>
2018-02-07 10:44:32 -0800

    implement an example of a non-monoid semigroup

--- chapter07/src/test/scala/example/Chapter07_01_examplesSpec.scala
+++ chapter07/src/test/scala/example/Chapter07_01_examplesSpec.scala
@@ -1,7 +1,9 @@
 package example
 
-import cats.{Functor, derive}
+import cats.kernel.Semigroup
+import cats.{Functor, Monoid, derive}
 import cats.syntax.functor._
+import cats.syntax.semigroup._
 import org.scalatest.FlatSpec
 
 class Chapter07_01_examplesSpec extends FlatSpec with CatsLawChecking {
@@ -9,7 +11,20 @@ class Chapter07_01_examplesSpec extends FlatSpec with CatsLawChecking {
   behavior of "examples"
 
   it should "check semigroup laws for a non-monoid type" in {
-    ???
-  }
-}
 
+    type TripleM[T] = (T, T, T)
+    // If P is a monoid type then we can define P × P × P as a semigroup in a special way.
+    implicit def tripleMonoid[P: Monoid]: Semigroup[TripleM[P]] = { (x: TripleM[P], y: TripleM[P]) ⇒ (x._1, x._2 combine y._2, y._3) }
+
+    // Use the Int monoid as an example.
+
+    implicit val monoidInt: Monoid[Int] = new Monoid[Int] {
+      override def empty: Int = 0
+
+      override def combine(x: Int, y: Int): Int = x + y
+    }
+
+    checkCatsSemigroupLaws[TripleM[Int]]()
+  }
+
+}
