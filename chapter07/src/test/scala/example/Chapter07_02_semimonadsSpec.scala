fef34a6830c8996f8c4ca4a7b6219b4a10b445ed
sergei.winitzki <sergei.winitzki@workday.com>
2018-05-12 21:25:51 -0700

    implement first monad construction

--- chapter07/src/test/scala/example/Chapter07_02_semimonadsSpec.scala
+++ chapter07/src/test/scala/example/Chapter07_02_semimonadsSpec.scala
@@ -1,5 +1,6 @@
 package example
 
+import cats.Monad
 import org.scalatest.FlatSpec
 
 class Chapter07_02_semimonadsSpec extends FlatSpec with FlattenableLawChecking with CatsLawChecking {
@@ -7,6 +8,17 @@ class Chapter07_02_semimonadsSpec extends FlatSpec with FlattenableLawChecking w
   behavior of "constructions of monads"
   
   it should "verify monad laws for constant functor" in {
+    type C[Z, A] = Z
+    // The functor is going to be C[Z, ?], for a fixed Z.
+    implicit def semimonadC[Z]: Semimonad[C[Z, ?]] = new Semimonad[C[Z, ?]] {
+      override def flatMap[A, B](fa: C[Z, A])(f: A ⇒ C[Z, B]): C[Z, B] = fa
+    }
     
+    // Flatten is id. So, associativity is trivial.
+    
+    // Why is this not a full monad? Because the right identity law fails unless Z = 1.
+    // We must have flatten (pure (x: C[Z, A])) = x. But pure: A ⇒ Z must give a fixed value of Z.
+    // So pure(x) cannot depend on x, and we cannot restore x if we only know pure(x).
+    // Therefore, flatten(pure(x)) cannot compute x.
   }
 }
