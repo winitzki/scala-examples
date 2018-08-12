f36c99dc2d8b480d212e3f34e0252f95f7dbdeb9
sergei.winitzki <sergei.winitzki@workday.com>
2018-08-11 18:22:52 -0700

    add example of non-traversable functor

--- chapter09/src/test/scala/example/Chapter09_01_examplesSpec.scala
+++ chapter09/src/test/scala/example/Chapter09_01_examplesSpec.scala
@@ -1,8 +1,21 @@
 package example
 
 import org.scalatest.{FlatSpec, Matchers}
+import io.chymyst.ch._
 
 class Chapter09_01_examplesSpec extends FlatSpec with Matchers {
 
   behavior of "traversable functors"
+
+  it should "show that non-polynomial functors are not traversable" in {
+    def useTypeParams[E, R](): Unit = {
+
+      type L[A] = E ⇒ A
+      type F[A] = R ⇒ A
+
+      def seqs[A] = allOfType[L[F[A]] ⇒ F[L[A]]]
+
+      seqs[Int].length shouldEqual 0 // There are no implementations of this type signature.
+    }
+  }
 }
