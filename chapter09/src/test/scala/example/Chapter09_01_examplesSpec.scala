d657edf52095ea2eaf3af27c07ef6b72316ae334
sergei.winitzki <sergei.winitzki@workday.com>
2018-08-11 18:24:19 -0700

    more examples of non-applicative functors

--- chapter09/src/test/scala/example/Chapter09_01_examplesSpec.scala
+++ chapter09/src/test/scala/example/Chapter09_01_examplesSpec.scala
@@ -10,12 +10,19 @@ class Chapter09_01_examplesSpec extends FlatSpec with Matchers {
   it should "show that non-polynomial functors are not traversable" in {
     def useTypeParams[E, R](): Unit = {
 
-      type L[A] = E ⇒ A
-      type F[A] = R ⇒ A
+      type L[A] = E ⇒ A // Non-polynomial functor.
+      type F[A] = R ⇒ A // Applicative functor.
 
       def seqs[A] = allOfType[L[F[A]] ⇒ F[L[A]]]
 
-      seqs[Int].length shouldEqual 0 // There are no implementations of this type signature.
+      seqs.length shouldEqual 0 // There are no implementations of this type signature.
+
+      type F2[A] = (A, A) // Another applicative functor.
+
+      def seqs2[A] = allOfType[L[F2[A]] ⇒ F2[L[A]]]
+
+      seqs2.length shouldEqual 0 // There are no implementations of this type signature.
+
     }
   }
 }
