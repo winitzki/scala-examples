7b7200091336a656bf2d3fc7ec0f65ec578ef10d
sergei.winitzki <sergei.winitzki@workday.com>
2018-08-11 18:36:42 -0700

    fix example of non-traversable functors

--- chapter09/src/test/scala/example/Chapter09_01_examplesSpec.scala
+++ chapter09/src/test/scala/example/Chapter09_01_examplesSpec.scala
@@ -11,18 +11,31 @@ class Chapter09_01_examplesSpec extends FlatSpec with Matchers {
     def useTypeParams[E, R](): Unit = {
 
       type L[A] = E ⇒ A // Non-polynomial functor.
-      type F[A] = R ⇒ A // Applicative functor.
+      type F[A] = Option[A] // Applicative functor.
 
       def seqs[A] = allOfType[L[F[A]] ⇒ F[L[A]]]
 
-      seqs.length shouldEqual 0 // There are no implementations of this type signature.
+      seqs.length shouldEqual 1 // There are no implementations of this type signature.
+      seqs.head.lambdaTerm.prettyPrint shouldEqual "a ⇒ (None() + 0)"
 
       type F2[A] = (A, A) // Another applicative functor.
 
       def seqs2[A] = allOfType[L[F2[A]] ⇒ F2[L[A]]]
 
-      seqs2.length shouldEqual 0 // There are no implementations of this type signature.
+      seqs2.length shouldEqual 1 // There is exactly one implementation of this type signature.
+      seqs2.head.lambdaTerm.prettyPrint shouldEqual "a ⇒ Tuple2(b ⇒ a b._1, c ⇒ a c._2)"
 
+      // Check that Z × A is traversable.
+
+      type L3[A] = (Int, A) // Polynomial functor.
+      type F3[A] = R ⇒ A // Another applicative functor.
+
+      def seqs3[A] = anyOfType[L3[F3[A]] ⇒ F3[L3[A]]]()
+
+      seqs3.length shouldEqual 1 // There is exactly one implementation of this type signature.
+      seqs3.head.lambdaTerm.prettyPrint shouldEqual "a ⇒ b ⇒ Tuple2(a._1, a._2 b)"
     }
+
+    useTypeParams()
   }
 }
