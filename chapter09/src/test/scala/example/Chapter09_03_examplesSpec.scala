981a24634bb440dc93331c07b609be3cb9eefeea
sergei.winitzki <sergei.winitzki@workday.com>
2018-09-03 02:03:58 -0700

    implement counterexamples and start usage

---
+++ chapter09/src/test/scala/example/Chapter09_03_examplesSpec.scala
@@ -0,0 +1,17 @@
+package example
+
+import org.scalatest.{FlatSpec, Matchers}
+
+import cats.{Applicative, Bifunctor, Bitraverse, Functor}
+import org.scalatest.{FlatSpec, Matchers}
+import cats.syntax.functor._
+import cats.syntax.bifunctor._
+import cats.syntax.bitraverse._
+import WuZip.WuZipSyntax
+import Trav.TravSyntax
+import io.chymyst.ch._
+
+class Chapter09_03_examplesSpec extends FlatSpec with Matchers {
+
+  behavior of "usage examples"
+}
