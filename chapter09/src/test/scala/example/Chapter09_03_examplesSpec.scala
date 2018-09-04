76f159e3568a479cb0394d372e9c6b4d24efa488
sergei.winitzki <sergei.winitzki@workday.com>
2018-09-03 19:21:03 -0700

    implement depth-first traversal

--- chapter09/src/test/scala/example/Chapter09_03_examplesSpec.scala
+++ chapter09/src/test/scala/example/Chapter09_03_examplesSpec.scala
@@ -10,6 +10,7 @@ import cats.syntax.traverse._
 import cats.syntax.bitraverse._
 import WuZip.WuZipSyntax
 import Trav._
+import cats.data.State
 import io.chymyst.ch._
 
 class Chapter09_03_examplesSpec extends FlatSpec with Matchers {
@@ -46,17 +47,33 @@ class Chapter09_03_examplesSpec extends FlatSpec with Matchers {
     toList(l1) shouldEqual List(3, 2, 1)
   }
 
-  it should "fold a tree to aggregate data" in {
-    sealed trait Tree[A]
-    final case class Leaf[A](x: A) extends Tree[A]
-    final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+  sealed trait Tree[A]
 
-    implicit val functorTree: Functor[Tree] = new Functor[Tree] {
-      override def map[A, B](fa: Tree[A])(f: A ⇒ B): Tree[B] = fa match {
-        case Leaf(x) ⇒ Leaf(f(x))
-        case Branch(left, right) ⇒ Branch(map(left)(f), map(right)(f))
-      }
+  final case class Leaf[A](x: A) extends Tree[A]
+
+  final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+
+  implicit val functorTree: Functor[Tree] = new Functor[Tree] {
+    override def map[A, B](fa: Tree[A])(f: A ⇒ B): Tree[B] = fa match {
+      case Leaf(x) ⇒ Leaf(f(x))
+      case Branch(left, right) ⇒ Branch(map(left)(f), map(right)(f))
     }
+  }
+
+
+  val t1: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+  val t2: Tree[String] = Branch(Branch(Leaf("a"), Branch(Leaf("b"), Leaf("c"))), Leaf("d"))
+  /*             .
+               /  \
+              .   d
+     t2 =   /  \
+           a   .
+             /  \ 
+            b   c
+   */
+
+  it should "fold a tree to aggregate data" in {
 
     implicit val travTree: Trav[Tree] = new Trav[Tree] {
       override def seq[F[_] : WuZip : Functor, A](t: Tree[F[A]]): F[Tree[A]] = t match {
@@ -68,8 +85,6 @@ class Chapter09_03_examplesSpec extends FlatSpec with Matchers {
 
     // Product of squares of all integers in the tree.
 
-    val t1: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
-
     implicit val monoidInt: Monoid[Int] = new Monoid[Int] {
       override def empty: Int = 1
 
@@ -77,5 +92,51 @@ class Chapter09_03_examplesSpec extends FlatSpec with Matchers {
     }
 
     t1.foldMap(x ⇒ x * x) shouldEqual 36
+  }
+
+  // Use a state monad as the applicative effect.
+  type S[X] = State[Int, X]
+  val makeLabel: S[Int] = for {
+    s ← State.get
+    _ ← State.set(s + 1)
+  } yield s
+
+  implicit val wuzipState: WuZip[S] = new WuZip[S]() {
+    override def wu: S[Unit] = State.pure(())
+
+    override def zip[A, B](fa: S[A], fb: S[B]): S[(A, B)] = for {
+      x ← fa
+      y ← fb
+    } yield (x, y)
+  }
+
+  it should "decorate a tree with depth-first traversal order labels" in {
+    // Depth-first traversal.
+    implicit val travTree: Trav[Tree] = new Trav[Tree] {
+      override def seq[F[_] : WuZip : Functor, A](t: Tree[F[A]]): F[Tree[A]] = t match {
+        case Leaf(fa) ⇒ fa.map(Leaf.apply)
+        case Branch(left, right) ⇒
+          seq[F, A](left) zip seq[F, A](right) map { case (x, y) ⇒ Branch(x, y) }
+      }
+    }
+    
+    val result: S[Tree[(String, Int)]] = t2.trav[S, (String, Int)](label ⇒ makeLabel.map((label, _)))
+    // Run the State monad.
+    result.run(1).value._2 shouldEqual Branch(Branch(Leaf(("a", 1)), Branch(Leaf(("b", 2)), Leaf(("c", 3)))), Leaf(("d", 4)))
+  }
+
+  it should "decorate a tree with breadth-first traversal order labels" in {
+    // Breadth-first traversal.
+    implicit val travTree: Trav[Tree] = new Trav[Tree] {
+      override def seq[F[_] : WuZip : Functor, A](t: Tree[F[A]]): F[Tree[A]] = t match {
+        case Leaf(fa) ⇒ fa.map(Leaf.apply)
+        case Branch(left, right) ⇒
+          seq[F, A](left) zip seq[F, A](right) map { case (x, y) ⇒ Branch(x, y) }
+      }
+    }
+
+    val result: S[Tree[(String, Int)]] = t2.trav[S, (String, Int)](label ⇒ makeLabel.map((label, _)))
+    // Run the State monad.
+    result.run(1).value._2 shouldEqual Branch(Branch(Leaf(("a", 1)), Branch(Leaf(("b", 2)), Leaf(("c", 3)))), Leaf(("d", 4)))
   }
 }
