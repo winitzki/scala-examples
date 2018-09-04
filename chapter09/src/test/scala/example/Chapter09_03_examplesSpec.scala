3890fca77db45a7b305d9222062173d963e16def
sergei.winitzki <sergei.winitzki@workday.com>
2018-09-03 19:04:15 -0700

    first two usage examples

--- chapter09/src/test/scala/example/Chapter09_03_examplesSpec.scala
+++ chapter09/src/test/scala/example/Chapter09_03_examplesSpec.scala
@@ -1,17 +1,81 @@
 package example
 
 import org.scalatest.{FlatSpec, Matchers}
-
-import cats.{Applicative, Bifunctor, Bitraverse, Functor}
+import cats.{Applicative, Bifunctor, Bitraverse, Eval, Functor, Monoid, Traverse}
 import org.scalatest.{FlatSpec, Matchers}
 import cats.syntax.functor._
+import cats.instances._
 import cats.syntax.bifunctor._
+import cats.syntax.traverse._
 import cats.syntax.bitraverse._
 import WuZip.WuZipSyntax
-import Trav.TravSyntax
+import Trav._
 import io.chymyst.ch._
 
 class Chapter09_03_examplesSpec extends FlatSpec with Matchers {
 
   behavior of "usage examples"
+
+  it should "convert a traversable functor to List" in {
+    def toList[L[_] : Trav : Functor, C](fa: L[C]): List[C] = {
+      // Define List[A] as a monoid type and a constant functor.
+      type Z[B] = List[C]
+      implicit val functorZ: Functor[Z] = new Functor[Z] {
+        override def map[A, B](fa: Z[A])(f: A => B): Z[B] = fa
+      }
+      implicit val wuZipZ: WuZip[Z] = new WuZip[Z]() {
+        override def wu: Z[Unit] = Nil
+
+        override def zip[A, B](fa: Z[A], fb: Z[B]): Z[(A, B)] = fb ++ fa // Note opposite order!
+      }
+      fa.trav[Z, C](c ⇒ List(c))
+    }
+
+    // Check that this works.
+    type L[A] = (A, A, A)
+    implicit val functorL: Functor[L] = new Functor[L] {
+      override def map[A, B](fa: L[A])(f: A ⇒ B): L[B] =
+        (f(fa._1), f(fa._2), f(fa._3))
+    }
+    implicit val travL: Trav[L] = new Trav[L] {
+      override def seq[F[_] : WuZip : Functor, A](lfa: L[F[A]]): F[L[A]] =
+        lfa._1 zip lfa._2 zip lfa._3 map { case ((x, y), z) ⇒ (x, y, z) }
+    }
+
+    val l1: L[Int] = (1, 2, 3)
+    toList(l1) shouldEqual List(3, 2, 1)
+  }
+
+  it should "fold a tree to aggregate data" in {
+    sealed trait Tree[A]
+    final case class Leaf[A](x: A) extends Tree[A]
+    final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+    implicit val functorTree: Functor[Tree] = new Functor[Tree] {
+      override def map[A, B](fa: Tree[A])(f: A ⇒ B): Tree[B] = fa match {
+        case Leaf(x) ⇒ Leaf(f(x))
+        case Branch(left, right) ⇒ Branch(map(left)(f), map(right)(f))
+      }
+    }
+
+    implicit val travTree: Trav[Tree] = new Trav[Tree] {
+      override def seq[F[_] : WuZip : Functor, A](t: Tree[F[A]]): F[Tree[A]] = t match {
+        case Leaf(fa) ⇒ fa.map(Leaf.apply)
+        case Branch(left, right) ⇒
+          seq[F, A](left) zip seq[F, A](right) map { case (x, y) ⇒ Branch(x, y) }
+      }
+    }
+
+    // Product of squares of all integers in the tree.
+
+    val t1: Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+
+    implicit val monoidInt: Monoid[Int] = new Monoid[Int] {
+      override def empty: Int = 1
+
+      override def combine(x: Int, y: Int): Int = x * y
+    }
+
+    t1.foldMap(x ⇒ x * x) shouldEqual 36
+  }
 }
--- common/src/main/scala/example/Trav.scala
+++ common/src/main/scala/example/Trav.scala
@@ -1,7 +1,9 @@
 package example
 
 import cats.Functor
+import cats.kernel.Monoid
 import cats.syntax.functor._
+import cats.syntax.monoid._
 
 abstract class Trav[L[_] : Functor] {
   def seq[F[_] : WuZip : Functor, A](lfa: L[F[A]]): F[L[A]]
@@ -11,10 +13,33 @@ abstract class Trav[L[_] : Functor] {
 
 object Trav {
   // Convenience method: find a Trav evidence.
-  def apply[L[_]: Trav]: Trav[L] = implicitly[Trav[L]]
+  def apply[L[_] : Trav]: Trav[L] = implicitly[Trav[L]]
 
   implicit class TravSyntax[L[_] : Trav : Functor, F[_] : WuZip : Functor, A](lfa: L[F[A]]) {
     def seq: F[L[A]] = Trav[L].seq[F, A](lfa)
+  }
+
+  // Convert monoids to WuZip.
+  def monoidFunctor[Z: Monoid]: Functor[Lambda[X ⇒ Z]] =   new Functor[Lambda[X ⇒ Z]] {
+    override def map[A, B](fa: Z)(f: A ⇒ B): Z = fa
+  }
+  def monoidWuZip[Z: Monoid]: WuZip[Lambda[X ⇒ Z]] = {
+    implicit val functorZ = monoidFunctor[Z]
+    new WuZip[Lambda[X ⇒ Z]] {
+      override def wu: Z = Monoid[Z].empty
+
+      override def zip[A, B](fa: Z, fb: Z): Z = fa |+| fb
+    }
+  }
+
+  implicit class TravSyntax2[L[_] : Trav : Functor, A](la: L[A]) {
+    def trav[F[_] : WuZip : Functor, B](f: A ⇒ F[B]): F[L[B]] = Trav[L].trav[F, A, B](la)(f)
+
+    def foldMap[Z: Monoid](f: A ⇒ Z): Z = {
+      implicit val functorZ = monoidFunctor[Z]
+      implicit val wuZipZ = monoidWuZip[Z]
+      Trav[L].trav[Lambda[X ⇒ Z], A, Z](la)(f)
+    }
   }
 
 }
