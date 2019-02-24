729dd9a3210f06ac2ea08d7d69a9019ef794da3b
sergei.winitzki <sergei.winitzki@workday.com>
2019-02-23 19:13:14 -0800

    WIP monad transformer for single-value monads

--- chapter11/src/test/scala/example/Chapter11_01_examplesSpec.scala
+++ chapter11/src/test/scala/example/Chapter11_01_examplesSpec.scala
@@ -1,5 +1,7 @@
 package example
 
+import cats.{Functor, Monad, Monoid}
+import cats.syntax.functor._
 import org.scalatest.{FlatSpec, Matchers}
 
 class Chapter11_01_examplesSpec extends FlatSpec with Matchers {
@@ -12,7 +14,7 @@ class Chapter11_01_examplesSpec extends FlatSpec with Matchers {
 
       var orMonad: CatsMonad[OR] = new CatsMonad[OR] {
 
-        override def flatMap[A, B](fa: OR[A])(f: A ⇒ OR[B]): OR[B] = fa match {
+        def flatMap[A, B](fa: OR[A])(f: A ⇒ OR[B]): OR[B] = fa match {
           // Need to return Option[R ⇒ B]. We should avoid always returning `None`,
           // because that would violate the identity law `fa.flatMap(pure) = fa`.
           case None ⇒ None // This is the only possibility here.
@@ -32,7 +34,7 @@ class Chapter11_01_examplesSpec extends FlatSpec with Matchers {
             }
         }
 
-        override def pure[A](x: A): OR[A] = Some { _ ⇒ x }
+        def pure[A](x: A): OR[A] = Some { _ ⇒ x }
       }
     }
 
@@ -48,7 +50,7 @@ class Chapter11_01_examplesSpec extends FlatSpec with Matchers {
       // Implement the type signature of Monad for SR[?].
 
       val srMonad: CatsMonad[SR] = new CatsMonad[SR] {
-        override def flatMap[A, B](fa: SR[A])(f: A ⇒ SR[B]): SR[B] = { s ⇒
+        def flatMap[A, B](fa: SR[A])(f: A ⇒ SR[B]): SR[B] = { s ⇒
           val (s1, ra): (S, R ⇒ A) = fa(s)
           (s1, { r ⇒ // Have to use s or s1 here, since `r` is only available inside the closure.
             val a: A = ra(r)
@@ -57,7 +59,7 @@ class Chapter11_01_examplesSpec extends FlatSpec with Matchers {
           })
         }
 
-        override def pure[A](x: A): SR[A] = { s ⇒ (s, _ ⇒ x) }
+        def pure[A](x: A): SR[A] = { s ⇒ (s, _ ⇒ x) }
       }
 
       // We have not used `f` in computing the new `s` after flatMap(f).
@@ -86,7 +88,7 @@ class Chapter11_01_examplesSpec extends FlatSpec with Matchers {
       // Implement the type signature of Monad for OS[?].
 
       val osMonad: CatsMonad[OS] = new CatsMonad[OS] {
-        override def flatMap[A, B](fa: OS[A])(f: A ⇒ OS[B]): OS[B] = fa match {
+        def flatMap[A, B](fa: OS[A])(f: A ⇒ OS[B]): OS[B] = fa match {
           // Need to return Option[St[B]]. We should avoid always returning `None`,
           // because that would violate the identity law `fa.flatMap(pure) = fa`.
           case None ⇒ None // This is the only possibility here.
@@ -110,9 +112,69 @@ class Chapter11_01_examplesSpec extends FlatSpec with Matchers {
             }
         }
 
-        override def pure[A](x: A): OS[A] = Some { s ⇒ (s, x) }
+        def pure[A](x: A): OS[A] = Some { s ⇒ (s, x) }
       }
     }
   }
 
+  behavior of "monad transformers for single-value monads"
+
+  it should "implement monad transformer for EW monad" in {
+    def withParams[E, W: Monoid] = {
+
+      import cats.syntax.monoid._
+
+      type EW[A] = Either[E, (W, A)] // It could also be (W, Either[E, A]). Both are monads.
+      // W × (E + A) = W × E + W × A can be rewritten as E + W × A if we replace `E × W` by a new `E`.
+      // The Option, Writer, and Either monads are special cases of `EW`.
+
+      // Transformer: composed-inside.
+      type EWT[M[_], A] = M[EW[A]]
+
+      // Functor instance for EW.
+      implicit val functorEW: Functor[EW] = cats.derive.functor[EW]
+
+      // Implement `flatten` for `EW`.
+      def flatten[A]: EW[EW[A]] ⇒ EW[A] = {
+        case Left(e) ⇒ Left(e)
+        case Right((w, Left(e))) ⇒ Left(e)
+        case Right((w1, Right((w2, x)))) ⇒ Right((w1 |+| w2, x))
+      }
+
+      // Monad instance for EW.
+      implicit val monadEW: CatsMonad[EW] = new CatsMonad[EW] {
+        def flatMap[A, B](fa: EW[A])(f: A ⇒ EW[B]): EW[B] = flatten(fa.map(f))
+
+        def pure[A](x: A): EW[A] = Right((Monoid[W].empty, x))
+      }
+
+      // Functor instance for EWT.
+      //      implicit def functorEWT[M[_] : Functor]: Functor[EWT[M, ?]] = new Functor[EWT[M, ?]] {
+      //        def map[A, B](fa: EWT[M, A])(f: A ⇒ B): EWT[M, B] = ???
+      //      }
+
+      // Traverse instance for EW; this is needed to define the transformer monad.
+      implicit val travEW: Trav[EW] = new Trav[EW] {
+        def seq[F[_] : WuZip : Functor, A](lfa: EW[F[A]]): F[EW[A]] = lfa match {
+          case Left(e) ⇒ WuZip[F].pure(Left(e))
+          case Right((w, fa)) ⇒ fa.map(x ⇒ Right((w, x)))
+        }
+      }
+
+      import Trav.TravSyntax
+      import CatsMonad.CatsMonadSyntax
+
+      // Implement flatten for EWT.
+      def flatten[M[_] : CatsMonad : WuZip : Functor, A](mewmewa: M[EW[M[EW[A]]]]): EWT[M, A] = {
+        // The plan is to transform M[EW[M[EW[A]]]] into M[M[EW[EW[A]]]], then flatten M and EW.
+        mewmewa.flatMap { ewmewa: EW[M[EW[A]]] ⇒ // Will return M[EW[A]] here.
+          val mewewa: M[EW[EW[A]]] = ewmewa.seq // Using `Trav` instance.
+          val mewa: M[EW[A]] = mewewa.map(flatten) // Using `flatten` defined above for `EW`.
+          mewa
+        }
+      }
+      
+      // The monad laws for the monad EWT were already verified in Chapter 7.
+    }
+  }
 }
