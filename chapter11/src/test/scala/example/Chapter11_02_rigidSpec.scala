73135c1dadb7aab839dd651625d809ab9285ef4e
Sergei Winitzki <winitzki@gmail.com>
2019-03-03 11:08:28 -0800

    adding rigid monad typeclass

---
+++ chapter11/src/test/scala/example/Chapter11_02_rigidSpec.scala
@@ -0,0 +1,11 @@
+package example
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Chapter11_02_rigidSpec extends FlatSpec with Matchers {
+
+  behavior of "rigid functors and monads"
+
+  
+
+}
---
+++ common/src/main/scala/example/Rigid.scala
@@ -0,0 +1,27 @@
+package example
+
+import cats.Functor
+
+abstract class Rigid[F[_] : Functor] {
+  def fuseIn[A, B](kleisli: A ⇒ F[B]): F[A ⇒ B]
+
+  // These methods do not need to be defined by implementations.
+  // Any functor has a `fuseOut` method.
+  def fuseOut[A, B](app: F[A ⇒ B]): A ⇒ F[B] = a ⇒ Functor[F].map(app)(ab ⇒ ab(a))
+
+  // Law: `fuseIn andThen fuseOut = identity`
+
+  // Any rigid functor is pointed.
+  def point[A](a: A): F[A] = Functor[F].map(fuseIn[F[A], A](identity))(_ ⇒ a)
+}
+
+object Rigid {
+
+  def apply[F[_] : Rigid]: Rigid[F] = implicitly[Rigid[F]]
+
+  implicit class RigidSyntax[F[_] : Rigid, A, B](f: A ⇒ F[B]) {
+    def fuseIn: F[A ⇒ B] = Rigid[F].fuseIn(f)
+  }
+
+}
+
