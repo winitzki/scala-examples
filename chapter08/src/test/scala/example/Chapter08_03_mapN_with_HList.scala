e3087a18687d1f554e397190d81a7a5035a5670f
sergei.winitzki <sergei.winitzki@workday.com>
2021-04-11 12:04:44 +0200

    more work on HList and headOption with type

--- chapter08/src/test/scala/example/Chapter08_03_mapN_with_HList.scala
+++ chapter08/src/test/scala/example/Chapter08_03_mapN_with_HList.scala
@@ -66,7 +66,7 @@ object HList2 {
 
   private val example1: HL[Nothing] = HN
   private val example2: HL[Int |: Nothing] = 1 |: HN
-  private val example3: HL[Int |: (String |: Nothing)] = 1 |: "xyz" |: HN
+  private val example3: HL[Int |: String |: Nothing] = 1 |: "xyz" |: HN
 
   // Not clear how to develop a type notation for this. We are using GADTs to create a list at the type level.
 
@@ -120,7 +120,7 @@ object HList3 {
 // see https://apocalisp.wordpress.com/2010/07/06/type-level-programming-in-scala-part-6a-heterogeneous-list%C2%A0basics/
 object HList4 {
   sealed trait HList {
-//    final def |:[A](a: A): A |: this.type = new |:(a, this) // Does not work with pattern-matching in a subtle way. Need to copy this definition to each case class and substitute this.type by the concrete type each time!
+    //    final def |:[A](a: A): A |: this.type = new |:(a, this) // Does not work with pattern-matching in a subtle way. Need to copy this definition to each case class and substitute this.type by the concrete type each time!
   }
 
   final case class |:[H, Tail <: HList](head: H, tail: Tail) extends HList {
@@ -134,7 +134,7 @@ object HList4 {
   type HNil = HNil.type
 
   implicit final class PostfixHList[A](val x: A) extends AnyVal {
-    def :| : (A |: HNil) = x |: HNil
+    def :| : A |: HNil = x |: HNil
   }
 
   import scala.language.postfixOps
@@ -164,15 +164,88 @@ object HList4a {
     def :: : A :: HNil = x :: HNil
   }
 
+  // Shorter API to finish the list. Not sure if this is really helpful, but it works.
   val example1 = 1 :: "abc" :: true.::
   val x: Boolean :: HNil = example1.tail.tail
 
   val y: String = example1 match {
     case _ :: z :: _ ⇒ z
   }
+
+  def headOption(hl: HList): Option[Any] = hl match {
+    case x: ::[_, _] ⇒ Some(x.head)
+    case HNil ⇒ None
+  }
+
+  def typedHeadOption[A](hl: HList): Option[A] = hl match {
+    case x: ::[A, _] ⇒ Some(x.head) // This will not be checked!
+    case HNil ⇒ None
+  }
+
+}
+
+object HList4b {
+  sealed trait HList {
+    type Head
+    type Tail
+  }
+
+  final case class ::[H, T <: HList](head: H, tail: T) extends HList {
+    def ::[A](a: A): A :: H :: T = new ::(a, this)
+
+    override type Head = H
+    override type Tail = T
+  }
+
+  object :: {
+    def unapply[H, T <: HList](cons: ::[H, T]): Option[(cons.Head, cons.Tail)] = Some((cons.head, cons.tail))
+  }
+
+  final case object HNil extends HList {
+    def ::[A](a: A): A :: HNil = new ::(a, this)
+
+    override type Head = Nothing
+    override type Tail = Nothing
+  }
+
+  type HNil = HNil.type
+
+  val example0 = HNil
+  val example0a: HList = HNil
+  val example1: Int :: String :: Boolean :: HNil = 1 :: "abc" :: true :: HNil
+  val example1a: HList = 1 :: "abc" :: true :: HNil
+
+  def headOption(hl: HList): Option[hl.Head] = hl match {
+    case x: ::[_, _] ⇒ Some(x.head.asInstanceOf[hl.Head])
+    case HNil ⇒ None
+  }
+
+  //  def typedHeadOption[A](hl: HList)(implicit is: A =:= hl.Head): Option[A] = hl match {
+  //    case ::(x, _) ⇒ Some(x) // This will not be checked?
+  //    case HNil ⇒ None
+  //  }
+
 }
 
 class Chapter08_03_mapN_with_HList extends FlatSpec with Matchers {
+
+  it should "check pattern-matching for HList4a" in {
+    import example.HList4a._
+    y shouldEqual "abc"
+    headOption(example1) shouldEqual Some(1)
+    val r = typedHeadOption[String](example1) // Even though the correct type is Int, this compiles and runs.
+    r shouldEqual Some(1)
+  }
+
+  it should "check pattern-matching for HList4b" in {
+    import example.HList4b._
+    val r: Option[Int] = headOption(example1) // The type is inferred correctly now.
+    r shouldEqual Some(1)
+    val t  = headOption(example0a) // The type cannot be inferred.
+    t shouldEqual None
+    val u = headOption(example1a)
+    u shouldEqual Some(1)
+  }
 
   it should "define mapN using type-level list" in {
 
