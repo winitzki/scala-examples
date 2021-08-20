0ac4f4a24745dbc1d3d78b8a66a9a9d01f559dcf
sergei.winitzki <sergei.winitzki@workday.com>
2021-08-20 18:33:16 +0200

    simpler implementation of Relation

--- chapter10/src/test/scala/example/Chapter10_relations_Spec.scala
+++ chapter10/src/test/scala/example/Chapter10_relations_Spec.scala
@@ -1,66 +1,48 @@
 package example
 
-import org.scalacheck.Gen.Parameters
-import org.scalacheck.rng.Seed
-import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.{FlatSpec, Matchers}
 
 class Chapter10_relations_Spec extends FlatSpec with Matchers {
 
   object Rel {
-    val params = Parameters.default.withSize(100000).withInitialSeed(seed)
+    val maxCount = 10000
 
-    def seed = Seed.random()
-
-    def apply[A: Arbitrary, B: Arbitrary](r: (A, B) ⇒ Boolean): Rel[A, B] = new Rel[A, B] {
-      override def check(a: A, b: B): Boolean = r(a, b)
-    }
+    val boolseq: Seq[Boolean] = Seq(false, true)
+    val intseq: Seq[Int] = (1 to maxCount).flatMap(x ⇒ (-x to x by x))
   }
 
   import Rel._
 
-  abstract class Rel[A: Arbitrary, B: Arbitrary] {
-    self ⇒
+  abstract class Rel[A, B] {
+      self ⇒
     def check(a: A, b: B): Boolean
 
     final def apply(a: A, b: B): Boolean = check(a, b)
 
-    def find(a: A): Gen[B] = implicitly[Arbitrary[B]].arbitrary.filter(b ⇒ check(a, b))
+    def find(a: A): Seq[B]
 
-    def compose[C: Arbitrary](other: Rel[B, C]): Rel[A, C] = new Rel[A, C] {
-      def findB(a: A, c: C): Gen[B] = self.find(a).filter(b ⇒ self.check(a, b) && other.check(b, c))
+    def compose[C](other: Rel[B, C]): Rel[A, C] = new Rel[A, C] {
+      def findB(a: A, c: C): Seq[B] = self.find(a).filter(b ⇒ self.check(a, b) && other.check(b, c))
 
-      override def check(a: A, c: C): Boolean = findB(a, c).apply(params, seed).nonEmpty
+      override def check(a: A, c: C): Boolean = findB(a, c).nonEmpty
 
-      override def find(a: A): Gen[C] = self.find(a).flatMap(b ⇒ other.find(b).filter(c ⇒ other.check(b, c)))
+      override def find(a: A): Seq[C] = self.find(a).flatMap(b ⇒ other.find(b).filter(c ⇒ other.check(b, c)))
     }
-  }
-
-  def fun2relAB[A: Arbitrary, B: Arbitrary](f: A => B): Rel[A, B] = new Rel[A, B] {
-    override def check(a: A, b: B): Boolean = b == f(a)
   }
 
   behavior of "Relations"
 
-  it should "use Arbitrary interface to generate numbers" in {
-    val g = implicitly[Arbitrary[Int]].arbitrary.apply(params, seed)
-    g.nonEmpty shouldEqual true
-
-    val h = implicitly[Arbitrary[Int]].arbitrary.filter(x ⇒ x != 10).apply(params, seed)
-    h.nonEmpty shouldEqual true
-  }
-
   it should "compose relations" in {
     // An example of a many-to-many relation that is not equivalent to a function
-    val r1: Rel[Int, Int] =  new Rel[Int, Int] {
+    val r1: Rel[Int, Int] = new Rel[Int, Int] {
       override def check(a: Int, b: Int): Boolean = a % 2 == b % 3
 
-      override def find(a: Int): Gen[Int] = implicitly[Arbitrary[Int]].arbitrary.map(_ * 3 + (a % 2))
+      override def find(a: Int): Seq[Int] = intseq.map(_ * 3 + (a % 2))
     }
-    val r2: Rel[Int, Int] =  new Rel[Int, Int] {
+    val r2: Rel[Int, Int] = new Rel[Int, Int] {
       override def check(a: Int, b: Int): Boolean = a % 3 == b % 5
 
-      override def find(a: Int): Gen[Int] = implicitly[Arbitrary[Int]].arbitrary.map(_ * 5 + (a % 3))
+      override def find(a: Int): Seq[Int] = intseq.map(_ * 5 + (a % 3))
     }
     val r3: Rel[Int, Int] = r1 compose r2
 
@@ -76,7 +58,7 @@ class Chapter10_relations_Spec extends FlatSpec with Matchers {
     r2(6, 10) shouldEqual true
     r2(7, 11) shouldEqual true
 
-    r1.find(1).apply(params, seed).nonEmpty shouldEqual true
+    r1.find(1).nonEmpty shouldEqual true
 
     r3(1, 1) shouldEqual true
     r3(1, 2) shouldEqual false
